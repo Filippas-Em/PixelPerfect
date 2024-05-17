6fb010b23d411b1ce6868deeedf72a3978165f60
Filippas-Em <emfiman02@gmail.com>
2024-05-17 18:20:58 +0300

    Scroll Animation

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 })
+
+
+// scroll animations
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+
+        }
+    });
+});
+
+//selecting elements to be animated
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
